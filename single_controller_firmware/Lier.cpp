86dd91ef5eac79ef48469eb090eeef7901172791
Marc Hulscher <mjhulscher@gmail.com>
2017-03-19 13:10:45 +0100

    daily commit

--- single_controller_firmware/Lier.cpp
+++ single_controller_firmware/Lier.cpp
@@ -15,37 +15,11 @@ out of or in connection with the software or the use or other dealings in
 the software.
 */
 
-#include "Encoder.h"
-#include "GearMotor.h"
-#include "PID_v1.h"
+#include Lier.h
 
-struct pos3D{
-  float posX;
-  float posY;
-  float posZ;
-};
-
-#ifndef Lier_h_
-#define Lier_h_
-
-class Lier{
-  public:
-    Lier();
-    int setupLier();
-    int moveTo(int count);
-    void setPID();  // relate to position or speed
-    void update();  // update per timestep or something (observe error & adjust write value)
-  private:
-    // one PID element
-    // PID _pid;
-    // one GearMotor
-    GearMotor _motor;
-    // one Encoder
-    Encoder _encoder;
-    // position of the Lier
-    pos3D _position;
-    // spindle radius
-    float _radius;
-};
-
-#endif //Lier_h_
+Lier::Lier(){
+    _motor = GearMotor();
+    // _encoder =
+    // _position =
+    // _radius =
+}
